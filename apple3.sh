2e015b600a8906746f068bd1a0971d275223f27c
Beyang Liu <beyang.liu@gmail.com>
2016-08-10 01:49:55 -0700

    WIP: clean up apple3.sh

--- apple3.sh
+++ apple3.sh
@@ -1,23 +1,22 @@
 #!/usr/bin/env osascript
 
 tell application "System Events"
-     set listOfProcesses to (every application process where background only is false)
+  set listOfProcesses to (every application process where background only is false)
 end tell
 
 repeat with proc in listOfProcesses
-     set procName to (name of proc)
-     log procName
-     try
-     tell application procName
-     	  repeat with i from 1 to (count windows)
-	  	 log (name of window i) as string
-	  end repeat
-     end tell
-     on error e
-       -- Note: these only show up if they're visible
-       log "## ERROR: " & e
-       repeat with win in windows of proc
-         log "# a window: " & (name of win as string)
-       end repeat
-     end try
+  set procName to (name of proc)
+  log "(PROCESS) "& procName
+  try
+    tell application procName
+      repeat with i from 1 to (count windows)
+        log "(WINDOW) " & (name of window i) as string
+      end repeat
+    end tell
+  on error e
+    -- Fall back to listing all visible windows
+    repeat with win in windows of proc
+      log "(WINDOW) " & (name of win as string)
+    end repeat
+  end try
 end repeat
